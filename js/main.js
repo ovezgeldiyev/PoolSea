19fbe3c8ec07d720c7432d6d56dc534276fb1a47
ovezgeldiyev <ovezgeldiyevagabay2000@gmail.com>
2022-12-07 20:51:03 +0500

    asd

--- js/main.js
+++ js/main.js
@@ -157,7 +157,7 @@ let seconds = upgradeTime;
 const countdown = document.getElementById("countdown");
 if (countdown) {
   let countdownTimer = setInterval("timer()", 1000);
-  
+
   function timer() {
     let days = Math.floor(seconds / 24 / 60 / 60);
     let hoursLeft = Math.floor(seconds - days * 86400);
@@ -185,76 +185,79 @@ if (countdown) {
   }
 }
 
-
 // timer end
 
-// // scrollMagic
-// const intro = document.querySelector("#startPoint");
-// const controller = new ScrollMagic.Controller();
-// const duration = 2000;
-// let scene = new ScrollMagic.Scene({
-//   duration: duration,
-//   triggerElement: intro,
-//   triggerHook: 0.07,
-// })
+// scrollMagic
+const intro = document.querySelector("#startPoint");
+const controller = new ScrollMagic.Controller();
+const duration = 2000;
+let scene = new ScrollMagic.Scene({
+  duration: duration,
+  triggerElement: intro,
+  triggerHook: 0.07,
+})
 
-//   .setPin(intro)
-//   .addTo(controller);
+  .setPin(intro)
+  .addTo(controller);
 
-// // canvas start
-// const canvas = document.querySelector(".animation-scrolling");
-// const context = canvas.getContext("2d");
+// canvas start
+const canvas = document.querySelector(".animation-scrolling");
 
-// const generatePath = (index) => {
-//   if (html.getAttribute("data-theme") == "light") {
-//     return `./images/canvas/white/${(index || 1)
-//       .toString()
-//       .padStart(4, "0")}.jpg`;
-//   } else {
-//     return `./images/canvas/black/${(index || 1)
-//       .toString()
-//       .padStart(4, "0")}.jpg`;
-//   }
-// };
+if (canvas) {
+  const context = canvas.getContext("2d");
 
-// const speed = 0.6;
+  const generatePath = (index) => {
+    if (html.getAttribute("data-theme") == "light") {
+      return `./images/canvas/white/${(index || 1)
+        .toString()
+        .padStart(4, "0")}.jpg`;
+    } else {
+      return `./images/canvas/black/${(index || 1)
+        .toString()
+        .padStart(4, "0")}.jpg`;
+    }
+  };
 
-// const frameCount = parseInt(intro.getAttribute("data-frames"));
-// canvas.height = 1100;
-// canvas.width = 1940;
-// const img = new Image();
-// img.src = generatePath(1);
-// img.onload = function () {
-//   context.drawImage(img, 0, 0);
-// };
-// window.addEventListener("scroll", () => {
-//   const scrollTop = html.scrollTop;
-//   const maxScrollTop = html.scrollHeight - window.innerHeight;
-//   const scrollFraction = frameCount / duration;
-//   const frameIndex = Math.min(
-//     frameCount - 1,
-//     Math.floor(speed * scrollTop * scrollFraction)
-//   );
-//   requestAnimationFrame(() => updateImage(frameIndex));
-// });
+  const speed = 0.6;
 
-// const updateImage = (index) => {
-//   img.src = generatePath(index);
-//   context.drawImage(img, 0, 0);
-// };
+  const frameCount = parseInt(intro.getAttribute("data-frames"));
+  canvas.height = 1100;
+  canvas.width = 1940;
+  const img = new Image();
+  img.src = generatePath(1);
+  img.onload = function () {
+    context.drawImage(img, 0, 0);
+  };
+  window.addEventListener("scroll", () => {
+    const scrollTop = html.scrollTop;
+    const maxScrollTop = html.scrollHeight - window.innerHeight;
+    const scrollFraction = frameCount / duration;
+    const frameIndex = Math.min(
+      frameCount - 1,
+      Math.floor(speed * scrollTop * scrollFraction)
+    );
+    requestAnimationFrame(() => updateImage(frameIndex));
+  });
 
-// const preloadImages = () => {
-//   img.src = generatePath(1);
-//   img.onload = function () {
-//     context.drawImage(img, 0, 0);
-//   };
-//   for (let i = 1; i < frameCount; i++) {
-//     const img = new Image();
-//     img.src = generatePath(i);
-//   }
-// };
-// preloadImages();
-// // canvas end
+  const updateImage = (index) => {
+    img.src = generatePath(index);
+    context.drawImage(img, 0, 0);
+  };
+
+  const preloadImages = () => {
+    img.src = generatePath(1);
+    img.onload = function () {
+      context.drawImage(img, 0, 0);
+    };
+    for (let i = 1; i < frameCount; i++) {
+      const img = new Image();
+      img.src = generatePath(i);
+    }
+  };
+  preloadImages();
+}
+
+// canvas end
 
 // copy start
 const copyBtn = document.getElementById("copyBtn");
@@ -331,7 +334,10 @@ themeChange.onchange = function () {
 // themeChange end
 
 const warn = document.getElementById("warn");
-const warnClose = document.getElementById("warnClose");
-warnClose.onclick = () => {
-  warn.style.display = "none";
-};
+if(warn) {
+  const warnClose = document.getElementById("warnClose");
+  warnClose.onclick = () => {
+    warn.style.display = "none";
+  };
+}
+
