0e387c63ec263f0b016c809e3aa9b0d3044be395
Ahmetqaz <107630795+Ahmetqaz@users.noreply.github.com>
2022-12-08 06:13:21 -0800

    asd

--- js/main.js
+++ js/main.js
@@ -338,3 +338,14 @@ if(warn) {
   };
 }
 
+
+
+
+
+
+
+const sponsorTooltip = () =>{
+  const content = document.querySelector("#sponsorsTooltip")
+  content.classList.add("active");
+  setTimeout(() => content.classList.remove("active"), 1500);
+}
