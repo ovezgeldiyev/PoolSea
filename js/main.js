ee80d202ced71cdb7d5892d5ed79862c10514a36
ovezgeldiyev <ovezgeldiyevagabay2000@gmail.com>
2022-12-09 21:02:56 +0500

    qwe

--- js/main.js
+++ js/main.js
@@ -63,23 +63,32 @@ var menu = document.getElementById("menu");
 var menuBtn = document.getElementById("menuBtn");
 var body = document.body;
 var html = document.querySelector("html");
+let header = document.getElementById("header");
 
+window.onresize = ()=> {
+  menu.classList.remove("active");
+  menuBtn.classList.remove("active");
+  body.classList.remove("active");
+  header.classList.remove("active");
+}
 menuBtn.onclick = function () {
   menu.classList.toggle("active");
   menuBtn.classList.toggle("active");
   body.classList.toggle("active");
+  header.classList.toggle("active");
+
 };
 window.onclick = function (event) {
   if (event.target == menu) {
     menu.classList.remove("active");
     menuBtn.classList.remove("active");
     body.classList.remove("active");
+    header.classList.remove("active");
   }
 };
 // menu end
 
 // scroll start
-let header = document.getElementById("header");
 function scrollFunc() {
   if (window.pageYOffset >= 60) {
     header.classList.add("sticky");
